d93f0f2b83ea416ab941c74917055ebebfe03546
immber <jessiekrushing@hotmail.com>
2015-08-30 17:22:40 -0700

    added gems for dotenv and yelp

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,10 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
+require 'yelp'
+require 'dotenv'
+
+Dotenv.load
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
